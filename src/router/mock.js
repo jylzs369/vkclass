d37e3b10d34acbf77f49ea209d2efeb4428eb794
Ushio <jylzs369@163.com>
2017-06-23 15:29:44 +0800

    modify mock.js

--- src/router/mock.js
+++ src/router/mock.js
@@ -2,20 +2,53 @@ const Mock = require('mockjs')
 const Random = Mock.Random
 
 module.exports = function (app) {
-  app.get('/course', function (req, res, next) {
+  app.get('/home', function (req, res, next) {
+    let data = Mock.mock({
+      'studied|1-10': 1,
+      'total|1-10': 1,
+      'ranking|1-99.1-2': 1,
+      'course|1-10': 1,
+      'exam|1-10': 1
+    })
+    res.json(data)
+  }).get('/course', function (req, res, next) {
     let data = Mock.mock({
       'processing|2': [
         {
           'id|+1': 0,
-          'title': '@sentence(18)',
-          'banner': Random.image('375x60')
+          'title': '@csentence(6, 18)',
+          'banner': Random.image('375x80', '#894FC4', '#FFF', 'png', '!')
         }
       ],
       'finished|5': [
         {
           'id|+1': 0,
-          'title': '@sentence(18)',
-          'banner': Random.image('375x60')
+          'title': '@csentence(6, 18)',
+          'banner': Random.image('375x80', '#894FC4', '#FFF', 'png', '!')
+        }
+      ]
+    })
+    res.json(data)
+  }).get('/points', function (req, res, next) {
+    let data = Mock.mock({
+      'points|5': [
+        {
+          'id|+1': 1,
+          'content': '@cparagraph'
+        }
+      ],
+      'questions|5': [
+        {
+          'id|+1': 1,
+          'content': '@csentence',
+          'options': [
+            '正确',
+            '@csentence',
+            '@csentence',
+            '@csentence'
+          ],
+          'correct|1': '正确',
+          'state': 0
         }
       ]
     })
